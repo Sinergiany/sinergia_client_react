2e008cfc524c80478b71a12feff9eede3daa6fcb
eadeveloper <eddeveloper@hotmail.com>
2018-03-29 16:51:46 -0400

    adding img to slideShow

--- src/components/slideShow.jsx
+++ src/components/slideShow.jsx
@@ -3,6 +3,14 @@ import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 import logo from '../logo.svg';
+import SignUpSupporter from '../img/SignUpSupporter.png';
+import uno from '../img/01.jpg';
+import dos from '../img/02.jpg';
+import tres from '../img/03.jpg';
+import cuatro from '../img/04.jpg';
+import cinco from '../img/05.jpg';
+
+
 
 export default class Carouselshow extends React.Component{
 
@@ -14,23 +22,23 @@ export default class Carouselshow extends React.Component{
                     <p className="legend">Legend 1</p>
                 </div>
                 <div>
-                  <img src={logo} />
+                  <img src={SignUpSupporter} />
                     <p className="legend">Legend 2</p>
                 </div>
                 <div>
-                  <img src={logo} />
+                  <img src={uno} />
                     <p className="legend">Legend 3</p>
                 </div>
                 <div>
-                  <img src={logo} />
+                  <img src={dos} />
                     <p className="legend">Legend 4</p>
                 </div>
                 <div>
-                  <img src={logo} />
+                  <img src={tres} />
                     <p className="legend">Legend 5</p>
                 </div>
                 <div>
-                  <img src={logo} />
+                  <img src={cuatro} />
                     <p className="legend">Legend 6</p>
                 </div>
             </Carousel>
